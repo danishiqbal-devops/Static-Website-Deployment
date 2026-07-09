0d595d5ed7bfd77dc500a37a3a5b7b054ba9f919
Danish Iqbal <engineerdevops502@gmail.com>
2026-07-09 15:47:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+
+strings: [
+"Junior DevOps Engineer",
+"AWS Cloud Engineer",
+"Linux Administrator",
+"CI/CD Enthusiast"
+],
+
+typeSpeed:80,
+backSpeed:50,
+loop:true
+
+});
